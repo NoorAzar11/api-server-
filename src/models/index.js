f093984dcd78786c564e8bbd0da81fb38bf67b84
NoorAzar11 <nooraaz945@gamil.com>
2021-08-17 14:42:29 +0300

    class4update

--- src/models/index.js
+++ src/models/index.js
@@ -1,32 +1,34 @@
 'use strict';
 
-
 const POSTGRES_URI =  process.env.DATABASE_URI || "postgres://localhost:5432/noor";
 const { Sequelize, DataTypes } = require('sequelize');
 
 let sequelizeOptions = {};
 let sequelize = new Sequelize(POSTGRES_URI, sequelizeOptions);
-
-// const foodData = require('./food');
-
-const CollectionCrud=require('./collection-class');
+const Collection=require('./collection-class');
 const CSTSchema=require('./cst.schema')
 const FoodSchema=require('./food')
+const clothesSchema=require('./clothes')
 
 const cstModel = CSTSchema(sequelize, DataTypes);
 const foodModel = FoodSchema(sequelize, DataTypes);
+const clothmoda=clothesSchema(sequelize, DataTypes);
 
-customerModel.hasMany(foodModel, { foreignKey: 'customerId', sourceKey: 'id'});
-foodModel.belongsTo(customerModel, { foreignKey: 'customerId', targetKey: 'id'});
-
-
+cstModel.hasMany(foodModel, { foreignKey: 'customerId', sourceKey: 'id'});
+cstModel.hasMany(clothmoda, { foreignKey: 'customerId', sourceKey: 'id'});
+foodModel.belongsTo(cstModel, { foreignKey: 'customerId', targetKey: 'id'});
+clothmoda.belongsTo(cstModel, { foreignKey: 'customerId', sourceKey: 'id'});
 //export Collections 
-const customerCollection = new Collection(customerModel);
+const customerCollection = new Collection(cstModel);
 const foodCollection = new Collection(foodModel);
+const clothcollection = new Collection(clothmoda);
 
 module.exports = {
+
   db: sequelize,
-  Food: foodData(sequelize, DataTypes),
-  customerCollection:customerCollection,
-  foodCollection:foodCollection
+  CST:customerCollection,
+  Food:foodCollection,
+  Clothes:clothcollection
 };
+
+
